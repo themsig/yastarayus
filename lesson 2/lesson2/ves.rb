bf314df8548948d661503878e8e365bc4ed2702a
themsig <120124326+themsig@users.noreply.github.com>
2022-12-26 00:19:22 +0300

    исправил отступы

--- lesson 2/lesson2/ves.rb
+++ lesson 2/lesson2/ves.rb
@@ -4,7 +4,7 @@ puts 'Введите ваш рост:'
 user_weight = gets.strip.to_i
 ideal_weight = (user_weight - 110) * 1.15
 if ideal_weight > 0
-puts "Ваш идеальный вес = #{ideal_weight.to_i}"
+  puts "Ваш идеальный вес = #{ideal_weight.to_i}"
 else
-puts 'Ваш вес уже оптимальный'
+  puts 'Ваш вес уже оптимальный'
 end
